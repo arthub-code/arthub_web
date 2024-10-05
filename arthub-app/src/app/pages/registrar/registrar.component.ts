b2a15199d8c1f80a0f3533dcc0be2fc32d78b383
mistervb <dragonxdgames@gmail.com>
2024-10-05 16:41:24 -0300

    [FIX] Ajustes na páigna de cadastro: - Melhoria de código; - Ajuste na URL da chamada POST para o serviço de criação de conta; - Ajuste no tratamento e caputra de mensagens da API; - Ajuste nos nomes dos campos do objeto para cadastro de conta; - Ajuste nos valores do select de tipos de conta - html de login;

--- arthub-app/src/app/pages/registrar/registrar.component.ts
+++ arthub-app/src/app/pages/registrar/registrar.component.ts
@@ -13,41 +13,40 @@ import { HttpClient } from '@angular/common/http';
     HttpClientModule
   ],
   templateUrl: './registrar.component.html',
-  styleUrl: './registrar.component.scss'
+  styleUrls: ['./registrar.component.scss'] // misterbv: Boas práticas do angular, estilos em array, porque podem ter vários estilos;
 })
 export class RegistrarComponent {
-  accountCreatePerson: ResgisterAccount = new ResgisterAccount();
+  accountCreatePerson: RegisterAccount = new RegisterAccount();
   etapa = 1;
-  cliente!: HttpClient;
   classStepTwo: string = "none";
   classStepOne: string = "flex";
   confirmarSenha!: string;
   nome!: string;
   sobrenome!: string;
 
-  constructor(private clientes: HttpClient){
-    this.cliente = clientes;
-  }
+  constructor(private cliente: HttpClient) {} // mistervb: Aqui o HttpClient pode ser injetado diretamente
 
-  public PostRegisterAccount(){
-    this.accountCreatePerson.fullname = this.nome + ' ' + this.sobrenome
-    this.cliente.post('localhost:8080/useraccount/v1/public/requestCreationUserAccount', this.accountCreatePerson).subscribe(
-      response => {
-        alert("Seu email foi cadastro com sucesso");
+  public PostRegisterAccount() {
+    this.accountCreatePerson.fullName = this.nome + ' ' + this.sobrenome;
+    this.cliente.post('http://localhost:8080/useraccount/v1/public/requestCreationUserAccount', this.accountCreatePerson)
+    .subscribe(
+      (response: any) => {
+        const successMessage = response ? JSON.stringify(response) : "Seu email foi cadastrado com sucesso";
+        alert(successMessage);
       },
-      error => {
-        alert("Sua senha precisa ter mais caracteres");
+      (error: any) => {
+        const errorMessage = error["error"] ? JSON.stringify(error) : "Ocorreu um erro durante o cadastro.";
+        alert(errorMessage);
       }
-    )
+    );
   }
 
-  public NextOrBackStage(){
-    if(this.classStepOne === "flex"){
+  public NextOrBackStage() {
+    if (this.classStepOne === "flex") {
       this.classStepTwo = "flex";
       this.classStepOne = "none";
       this.etapa = 2;
-    } 
-    else{
+    } else {
       this.classStepTwo = "none";
       this.classStepOne = "flex";
       this.etapa = 1;
@@ -55,12 +54,12 @@ export class RegistrarComponent {
   }
 }
 
-export class ResgisterAccount{
-    email!: string;
-    socialName!: string;
-    username!: string;
-    password!: string;
-    userAccountType!: string;
-    fullname!: string;
-    dateOfBirth!: Date;
+export class RegisterAccount {
+  email!: string;
+  socialName!: string;
+  username!: string;
+  password!: string;
+  userAccountType!: string;
+  fullName!: string;
+  dateOfBirth!: Date;
 }
