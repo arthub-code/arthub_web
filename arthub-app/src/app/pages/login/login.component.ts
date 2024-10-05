c727541db1c16d915e275ec5d49676e1c0c25b24
Xand Sobral <alexandre.psobral@hotmail.com>
2024-10-05 15:03:40 -0300

    [ADD] Endpoint de login implementado - implementação de readme (escrita de manual de como usar o projeto) - implementação de endpoint de login

--- arthub-app/src/app/pages/login/login.component.ts
+++ arthub-app/src/app/pages/login/login.component.ts
@@ -1,15 +1,41 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
     RouterOutlet,
+    HttpClientModule,
+    FormsModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  accountLoginPerson: LoginPayload = new LoginPayload();
+  client!: HttpClient;
 
+  constructor(private http: HttpClient){
+    this.client = http;
+  }
+
+  public PostLoginAccount(){
+    this.client.post('localhost:8080/useraccount/v1/public/login', this.accountLoginPerson).subscribe(
+      response => {
+        alert("Seu email foi cadastro com sucesso");
+      },
+      error => {
+        alert("Sua senha precisa ter mais caracteres");
+      }
+    );
+  }
+}
+
+class LoginPayload{
+  email!: string;
+  password!: string;
 }
