482569775cc864e1af1c42afae369ffc74cfcf14
Xand Sobral <alexandre.psobral@hotmail.com>
2024-10-21 16:16:17 -0300

    [ADD] Estilização do switch - funcionalidade de action - animação

--- arthub-app/src/app/components/pages-components/home-page/content-artes-andamento/modals/modal-registrar-arte/modal-registrar-arte.component.ts
+++ arthub-app/src/app/components/pages-components/home-page/content-artes-andamento/modals/modal-registrar-arte/modal-registrar-arte.component.ts
@@ -8,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './modal-registrar-arte.component.scss'
 })
 export class ModalRegistrarArteComponent {
+  classButton: string = 'button-active';
+  classButtonStatus: boolean = false;
 
+  switchButton(){
+    if(!this.classButtonStatus){
+      this.classButton = 'button-desactive';
+      this.classButtonStatus = true;
+    }
+    else{
+      this.classButton = 'button-active';
+      this.classButtonStatus = false;
+    }
+  }
 }
